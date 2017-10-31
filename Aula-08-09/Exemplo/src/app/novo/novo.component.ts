780379546630c405a54d0298db4efd48cbc88695
Vitor Hugo <vhsilva.ap@gmail.com>
2017-10-31 19:17:42 -0200

    Slide aula 08-09

---
+++ Aula-08-09/Exemplo/src/app/novo/novo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'novo-component',
+  template: `
+  		<h1>Bem vindo</h1>
+  		<p>bla</p>
+  `
+})
+export class NovoComponent {
+}

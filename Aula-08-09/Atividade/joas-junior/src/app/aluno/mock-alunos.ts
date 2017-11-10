7f21084be67475430d0a3a08796e70ef0edfb6e4
joasribeiro <joas036@gmail.com>
2017-11-10 01:38:40 -0300

    atividade enviada?

---
+++ Aula-08-09/Atividade/joas-junior/src/app/aluno/mock-alunos.ts
@@ -0,0 +1,10 @@
+import { Aluno } from './aluno';
+
+export const ALUNOS: Aluno[] = [
+
+{ id : 2, nome: 'joas' },
+{ id: 3, nome : 'junior' },
+{ id: 4, nome : 'ribeiro' },
+{ id: 5, nome : 'silva' }
+
+];

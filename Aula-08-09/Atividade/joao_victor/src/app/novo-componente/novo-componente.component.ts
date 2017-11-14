c08b6e099aa31bd8aba43a1ae08440e733094edd
joaovc <joaovcsantosap@gmail.com>
2017-11-14 17:15:15 -0200

    ATI Angula

---
+++ Aula-08-09/Atividade/joao_victor/src/app/novo-componente/novo-componente.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'novo-componente',
+  template:`
+  <div>
+    <h1>Novo Componente!!!</h1>
+  </div>
+`
+})
+
+export class NovoComponente {
+
+}

3aaadeae1f05951cd66bf096a0708eb216900b81
AmericoRodrigues <americorodrigues82@gmail.com>
2017-11-21 17:54:19 -0300

    Ativide do inferno 😒

---
+++ Aula-10/Atividade/AmericoRodrigues/aula10/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titlep = 'Lista de Professores';
+  titlea = 'Lista de Alunos';
+}
---
+++ Aula-10/Atividade/AmericoRodrigues/aula10/src/app/professores/mock-professores.ts
@@ -0,0 +1,8 @@
+import { Professor } from './professores';
+
+export const PROFESSORES: Professor[] = [ 
+	{ id: 2, nome: 'Paulo Tarciso'},
+	{ id: 3, nome: 'Allan David'},
+	{ id: 4, nome: 'Alan Cunha'},
+	{ id: 5, nome: 'Robson Dias'}
+];

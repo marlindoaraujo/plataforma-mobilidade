569b7fa0e82a9c25779af61db8c1c1852c40c3b5
joasribeiro <joas036@gmail.com>
2017-12-06 01:15:32 -0300

    ativ 11 :bowtie: y

---
+++ Aula-11/Atividade/joas-junior/src/app/professor/mock-professores.ts
@@ -0,0 +1,9 @@
+import { Professor } from './professor'; // Importando nossa classe de modelo de Aluno
+
+export const PROFESSORES: Professor[] = [ //Criando um aconstate com um array de Objetos do tipo Aluno
+    {  nome: 'junior'},
+    {  nome: 'silva'},
+    {  nome: 'ribeiro'},
+    {  nome: 'joas'},
+    {  nome: 'haha'},
+];

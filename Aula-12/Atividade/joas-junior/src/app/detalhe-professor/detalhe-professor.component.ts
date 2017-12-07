346ce0921f4edc950ce9e0dde941c3a8158a47e3
joasribeiro <joas036@gmail.com>
2017-12-07 20:16:02 -0300

    ativ aula :one::two: :white_check_mark:

---
+++ Aula-12/Atividade/joas-junior/src/app/detalhe-professor/detalhe-professor.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; 
+import { Location } from '@angular/common';
+
+import { Professor } from '../professor/professor';
+import { ProfessorService } from '../professor.service';
+
+@Component({
+  selector: 'app-detalhe-professor',
+  templateUrl: './detalhe-professor.component.html',
+  styleUrls: ['./detalhe-professor.component.css']
+})
+export class DetalheProfessorComponent implements OnInit {
+
+  @Input() professor: Professor;
+
+  constructor(
+  	private route: ActivatedRoute,
+  	private professorService: ProfessorService,
+  	private location: Location) { } 
+
+  ngOnInit() {
+  	this.getProfessor();
+  }
+
+  getProfessor(): void {
+	  const id = +this.route.snapshot.paramMap.get('id');
+	  this.professorService.getProfessor(id)
+	    .subscribe(professor => this.professor = professor);
+  }
+
+  goBack(): void {
+  	this.location.back(); //função para voltar a URL anterior
+  }
+
+}

d9b8f3c8b75fa36a95ce748b6345156e3455978b
wevertonrego <weverton.belmont@hotmail.com>
2017-12-08 18:25:41 -0300

    atividade 12

--- Aula-12/Atividade/weverton/src/app/app.module.ts
+++ Aula-12/Atividade/weverton/src/app/app.module.ts
@@ -4,20 +4,20 @@
 
 
   import { AppComponent } from './app.component';
-  import { AlunoComponent } from './aluno/aluno.component';
-  import { DetalheAlunoComponent } from './detalhe-aluno/detalhe-aluno.component';
+  import { ProfessorComponent } from './professor/professor.component';
+  import { DetalheProfessorComponent } from './detalhe-professor/detalhe-professor.component';
 
-  import { AlunoService } from './aluno.service'; //Importa nosso Service de alunos
+  import { ProfessorService } from './professor.service';
   import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component'; //Importa nosso Service de mensagens
+import { DashboardComponent } from './dashboard/dashboard.component';
 
   @NgModule({
     declarations: [
       AppComponent,
-      AlunoComponent,
-      DetalheAlunoComponent,
+      ProfessorComponent,
+      DetalheProfessorComponent,
       MessagesComponent,
       DashboardComponent
     ],
@@ -26,7 +26,7 @@ import { DashboardComponent } from './dashboard/dashboard.component'; //Importa 
       FormsModule,
       AppRoutingModule
     ],
-    providers: [ AlunoService, MessageService ], //Adiciona nossas Serviços ao providers do nosso módulo princial
+    providers: [ ProfessorService, MessageService ],
     bootstrap: [AppComponent]
   })
   export class AppModule { }

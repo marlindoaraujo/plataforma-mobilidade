50705a0781e856ecc0ffdb9ef4189fca20ee2414
AmericoRodrigues <americorodrigues82@gmail.com>
2017-12-08 16:51:00 -0300

    atualizando

--- Aula-12/Atividade/americo/src/app/professor/mock-professores.ts
+++
@@ -1,9 +0,0 @@
-import { Professor } from './professor';
-
-export const PROFESSORES: Professor[] = [
-    { id: 2, nome: 'Paulo de Tarço'},
-    { id: 3, nome: 'Paulo Tarciso'},
-    { id: 4, nome: 'Allan David'},
-    { id: 5, nome: 'Alan Cunha '},
-    { id: 6, nome: 'Ronedo'},
-];

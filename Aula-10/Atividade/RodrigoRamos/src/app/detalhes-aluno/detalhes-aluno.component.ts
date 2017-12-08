5d7ddabf081ed847d26ade807b66ead85c9d3754
RRodrigoRamos <systemas.rodrigo@gmail.com>
2017-12-08 18:17:44 -0200

    Atividades

---
+++ Aula-10/Atividade/RodrigoRamos/src/app/detalhes-aluno/detalhes-aluno.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { Aluno } from '../aluno/aluno';
+
+@Component({
+  selector: 'app-detalhes-aluno',
+  templateUrl: './detalhes-aluno.component.html',
+  styleUrls: ['./detalhes-aluno.component.css']
+})
+export class DetalhesAlunoComponent implements OnInit {
+
+  @Input() aluno: Aluno;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

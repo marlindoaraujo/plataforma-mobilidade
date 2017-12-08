ecd8a3339328612b19d35b03cc73efc8ceb30e66
janaina19 <cardosojanaina30@gmail.com>
2017-12-08 19:08:27 -0300

    corrigindo att-12

--- Aula-12/Atividade/janaina/src/app/dashboard/dashboard.component.ts
+++ Aula-12/Atividade/janaina/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Aluno } from '../aluno/aluno'; //importe nosso modelo de dados de Aluno
-import { AlunoService } from '../aluno.service'; //Importe nosso service de Aluno
+import { Professor } from '../professor/professor';
+import { ProfessorService } from '../professor.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,18 +9,17 @@ import { AlunoService } from '../aluno.service'; //Importe nosso service de Alun
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  
-  alunos: Aluno[] = []; //crie uma propriedade chamada alunos do tipo array de Aluno com valor de uma rray vazio
 
-  constructor(private alunoService: AlunoService) { }/*Injete a uma propriedade
-  privada na nossa classe chamada alunoService do tipo AlunoService*/
+  professores: Professor[] = [];
+
+  constructor(private professorService: ProfessorService) { }
 
   ngOnInit() {
-  	this.getAlunos(); //Executa o método assim que instância de DashboardComponent for carregada
-  }
+  	this.getProfessores();
+    }
 
-  getAlunos(): void {
-  	this.alunoService.getAlunos()
-  	.subscribe(alunos => this.alunos = alunos.slice(0,4)); // Carregue os 4 primeiro alunos da lista
+  getProfessores(): void {
+  	this.professorService.getProfessores()
+  	.subscribe(professores => this.professores = professores.slice(0,4));
   }
 }
--- Aula-12/Atividade/janaina/src/app/detalhe-professor/detalhe-professor.component.spec.ts
+++ Aula-12/Atividade/janaina/src/app/detalhe-professor/detalhe-professor.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DetalheAlunoComponent } from './detalhe-aluno.component';
+import { DetalheProfessorComponent } from './detalhe-professor.component';
 
-describe('DetalheAlunoComponent', () => {
-  let component: DetalheAlunoComponent;
-  let fixture: ComponentFixture<DetalheAlunoComponent>;
+describe('DetalheProfessorComponent', () => {
+  let component: DetalheProfessorComponent;
+  let fixture: ComponentFixture<DetalheProfessorComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetalheAlunoComponent ]
+      declarations: [ DetalheProfessorComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetalheAlunoComponent);
+    fixture = TestBed.createComponent(DetalheProfessorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

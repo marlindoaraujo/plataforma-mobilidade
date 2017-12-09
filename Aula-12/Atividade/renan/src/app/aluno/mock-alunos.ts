fc4f57fb23696b365bbc553bc2b63a699c5c3450
Renan Almeida <renan.webtec@hotmail.com>
2017-12-09 17:07:17 -0300

    Entrega de atividade-12

---
+++ Aula-12/Atividade/renan/src/app/aluno/mock-alunos.ts
@@ -0,0 +1,9 @@
+import { Aluno } from './aluno'; // Importando nossa classe de modelo de Aluno
+
+export const ALUNOS: Aluno[] = [ //Criando um aconstate com um array de Objetos do tipo Aluno
+    { id: 1, nome: 'Anderson'},
+    { id: 2, nome: 'Weslley'},
+    { id: 3, nome: 'Milton'},
+    { id: 4, nome: 'Renan'},
+    { id: 5, nome: 'João'},
+];

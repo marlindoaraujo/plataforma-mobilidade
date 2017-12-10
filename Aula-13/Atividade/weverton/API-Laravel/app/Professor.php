0cfbfbd471a958b01d576c3f6e0bd9dd7f59abd3
joasribeiro <joas036@gmail.com>
2017-12-10 14:19:33 -0300

    entregando atividade aula 13 vlw

---
+++ Aula-13/Atividade/weverton/API-Laravel/app/Professor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Professor extends Model
+{
+	protected $table = 'professores';
+	
+    protected $fillable = [
+    	'nome'
+    ];
+}

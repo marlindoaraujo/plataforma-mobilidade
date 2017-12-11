c1d6898e8eb3a6f50a2d9dfcc008af1e4359a5a0
Erison Santos <erisonsantos111@gmail.com>
2017-12-11 14:41:14 -0200

    atividade 12 aff

---
+++ Aula-12/Atividade/Erison_Santos/src/app/professor/mock-professores.ts
@@ -0,0 +1,9 @@
+import { Professor } from './professor';
+
+export const PROFESSORES: Professor[] = [
+    { id: 2, nome: 'Allan'},
+    { id: 3, nome: 'Paulo'},
+    { id: 4, nome: 'Tarciso'},
+    { id: 5, nome: 'Vitor'},
+    { id: 6, nome: 'Ronedo'},
+];

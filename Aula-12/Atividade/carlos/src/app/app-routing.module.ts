affa3add99ddb222f46ed20b52ee6a5437052a82
Renan Almeida <renan.webtec@hotmail.com>
2017-12-12 03:13:05 -0300

    Entrega de atividade 12 - by: Carlos Eduardo

---
+++ Aula-12/Atividade/carlos/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+
+import { AlunoComponent } from './aluno/aluno.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetalheAlunoComponent } from './detalhe-aluno/detalhe-aluno.component'; //importa o  DetalheAlunoComponent 
+
+const routes: Routes = [
+	{ path: 'alunos', component: AlunoComponent },
+	{ path: 'aluno/:id', component: DetalheAlunoComponent }, /* Difine a rota os :id 
+	indica que esse será um parâmentro a ser passado similar aos paramentros na 
+	roda do laravel aluno/{id}*/
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  exports: [ RouterModule  ],
+  imports: [ RouterModule.forRoot(routes) ]
+})
+export class AppRoutingModule { }

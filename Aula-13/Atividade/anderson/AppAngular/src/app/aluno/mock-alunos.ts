fc637a538fba700fa8cfb2f6d51e6b137e326a54
Andersonpinheirocruz <anderson95pinheiro@gmail.com>
2017-12-12 18:44:10 -0300

    atividade 13

---
+++ Aula-13/Atividade/anderson/AppAngular/src/app/aluno/mock-alunos.ts
@@ -0,0 +1,9 @@
+import { Aluno } from './aluno'; // Importando nossa classe de modelo de Aluno
+
+export const ALUNOS: Aluno[] = [ //Criando um aconstate com um array de Objetos do tipo Aluno
+    { id: 2, nome: 'Paulo de Tarço'},
+    { id: 3, nome: 'Vitor Hugo'},
+    { id: 4, nome: 'Ronedo Sá'},
+    { id: 5, nome: 'Paulo Tarciso'},
+    { id: 6, nome: 'Allan David'},
+];
